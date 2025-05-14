3afcee498f2b2a7b180ddfbba7a285c2715d47b0
ErmondBeqiri <Ermondbeqiri171@gmail.com>
2025-05-14 13:37:58 +0200

    Add files via upload

---
+++ classe/classe/FeldGenerrirenm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace classe
+{
+    class FeldGenerrirenm
+    {
+        Button button = new Button();
+
+        public Button Button
+        {
+            get
+            {
+                return button;
+
+            }
+            set
+            {
+
+
+                button = value;
+            }
+
+
+        }
+        public FeldGenerrirenm()
+        {
+            button.Width = 80;
+            button.Height = 80;
+            button.Background = new SolidColorBrush(Colors.Black);
+            button.FontFamily = new FontFamily("Papyrus");
+            button.FontSize = 30;
+            button.BorderBrush = Brushes.White;
+            button.BorderThickness = new Thickness(2, 2, 2, 2);
+            button.Content = "";
+
+
+        }
+
+    }
+}
+
---
+++ classe/classe/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace classe
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            hinzufuegen();
+        }
+
+        FeldGenerrirenm[] feldgeneriren = new FeldGenerrirenm[9];
+
+        public void hinzufuegen()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                feldgeneriren[i] = new FeldGenerrirenm();
+                Box1.Children.Add(feldgeneriren[i].Button);
+            }
+        }
+    }
+}
